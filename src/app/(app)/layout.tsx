89f76de51fa777a2d0c34a2d94a6785661668a7a
Imoudu Erua <imouduerua@gmail.com>
2025-12-19 19:38:05 +0000

    look at what you're doing

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import { AppSidebar } from "@/components/layout/app-sidebar";
 import { AppHeader } from "@/components/layout/header";
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { UserDataProvider } from "@/context/user-data-provider";
 
 export default function AppLayout({
@@ -13,7 +13,7 @@ export default function AppLayout({
       <UserDataProvider>
         <div className="min-h-screen bg-background flex">
           <AppSidebar />
-          <div className="flex-1 flex flex-col">
+          <div className="flex-1 flex flex-col w-full overflow-x-hidden">
             <AppHeader />
             <main className="flex-1 overflow-y-auto">
                 <div className="w-full">
