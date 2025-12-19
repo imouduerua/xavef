fed6c63eacafd6da533242836bc6bc2819ffdd53
Imoudu Erua <imouduerua@gmail.com>
2025-12-19 20:23:13 +0000

    Still thesame thing the sapace is still there

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -13,7 +13,7 @@ export default function AppLayout({
       <UserDataProvider>
         <div className="flex min-h-screen bg-background">
           <AppSidebar />
-          <div className="w-full flex flex-col">
+          <div className="flex-1 flex flex-col">
             <AppHeader />
             <main className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">
               {children}
