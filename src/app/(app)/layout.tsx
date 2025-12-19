c79639a907e77d5849961b8ef311022ebaba965d
Imoudu Erua <imouduerua@gmail.com>
2025-12-19 20:21:31 +0000

    the avialable space isn't the one close to the sidebar, it on the other

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -13,7 +13,7 @@ export default function AppLayout({
       <UserDataProvider>
         <div className="flex min-h-screen bg-background">
           <AppSidebar />
-          <div className="flex flex-1 flex-col">
+          <div className="w-full flex flex-col">
             <AppHeader />
             <main className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">
               {children}
