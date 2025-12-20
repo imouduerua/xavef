acd35505523c733b3ac504970c36e647778ad6af
Imoudu Erua <imouduerua@gmail.com>
2025-12-20 08:26:57 +0000

    Try fixing this error: `Runtime ReferenceError: FormDescription is not d

--- src/components/dashboard/deposit-dialog.tsx
+++ src/components/dashboard/deposit-dialog.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -20,6 +21,7 @@ import {
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
