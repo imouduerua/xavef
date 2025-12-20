c768c983f66d522c03df19b774decdfb49bf3813
Imoudu Erua <imouduerua@gmail.com>
2025-12-20 08:25:49 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,17 +1,14 @@
-import Image from 'next/image';
+import { Shield } from 'lucide-react';
 
-export const XavefLogo = (props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) => (
-  <Image src="/logo.png" alt="Xavef Financials Logo" width={64} height={64} {...props} />
+export const XavefLogo = (props: React.ComponentProps<typeof Shield>) => (
+  <Shield {...props} />
 );
 
-export const XavefLogoText = (props: Omit<React.ComponentProps<"div">, "children">) => (
-    <div {...props}>
-        <Image
-            src="/logo.png"
-            alt="Xavef Financials"
-            width={120}
-            height={30}
-            className="h-8 w-auto dark:invert"
-        />
-    </div>
+export const XavefLogoText = (
+  props: Omit<React.ComponentProps<'div'>, 'children'>
+) => (
+  <div {...props} className="flex items-center gap-2">
+    <Shield className="h-8 w-8" />
+    <span className="text-xl font-bold">XAVEF</span>
+  </div>
 );
