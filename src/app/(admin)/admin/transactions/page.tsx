7d3cd3683ad9ba0a0d5742914a07757e409e223e
Imoudu Erua <imouduerua@gmail.com>
2025-12-21 20:16:30 +0000

    On the all transaction page (## Error Type Console FirebaseError
    ## Er

--- src/app/(admin)/admin/transactions/page.tsx
+++ src/app/(admin)/admin/transactions/page.tsx
@@ -101,7 +101,7 @@ function AllTransactionsPageContent() {
 
     processTransactions();
   // We only want to re-run this effect when the raw, unprocessed data changes.
-  }, [rawTransactions, firestore, transactions]);
+  }, [rawTransactions, firestore, transactions, indexCreationUrl]);
 
   const renderContent = () => {
     // Priority 1: Show index creation alert if needed.
