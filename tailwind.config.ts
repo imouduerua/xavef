d54306f6bd13a4266e884f4b99bf0e6d620c3c5f
Imoudu Erua <imouduerua@gmail.com>
2026-01-01 10:34:19 +0000

    nothing is changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['var(--font-inter)', 'sans-serif'],
-        headline: ['var(--font-inter)', 'sans-serif'],
+        body: ['var(--font-pacifico)', 'sans-serif'],
+        headline: ['var(--font-pacifico)', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
