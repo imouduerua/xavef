3705896e9b65036337964e65ae1e1f7918791a90
Imoudu Erua <imouduerua@gmail.com>
2026-01-12 11:22:14 +0000

    Registration Error. Server is not configured for database access.Please

--- src/firebase/server-init.ts
+++ src/firebase/server-init.ts
@@ -14,15 +14,16 @@ let firestore: Firestore | undefined;
 
 // Per best practice, we only initialize the app once.
 // In this environment, server credentials are not available, so we will not initialize the admin app.
-// if (!getApps().length) {
-//     // This will throw an error during server startup if GOOGLE_APPLICATION_CREDENTIALS
-//     // is not set, which is the desired "fail-fast" behavior. This prevents the server
-//     // from running in a misconfigured state.
-//     app = initializeApp();
-// } else {
-//     app = getApp();
-// }
+if (!getApps().length) {
+    // This will throw an error during server startup if GOOGLE_APPLICATION_CREDENTIALS
+    // is not set, which is the desired "fail-fast" behavior. This prevents the server
+    // from running in a misconfigured state.
+    app = initializeApp();
+} else {
+    app = getApp();
+}
 
-// firestore = getFirestore(app);
+firestore = getFirestore(app);
 
 export { app, firestore };
+
