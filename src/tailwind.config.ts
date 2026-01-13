b52ef0ab2ab31b76832731fef0823e8714d00381
Imoudu Erua <imouduerua@gmail.com>
2026-01-13 15:05:52 +0000

    I want the buttons and lettering to be dark on light themes and light on

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
     },
     extend: {
       fontFamily: {
-        sans: ['var(--font-share-tech)', 'sans-serif'],
+        sans: ['var(--font-sans)', 'sans-serif'],
       },
       colors: {
         border: 'hsl(var(--border))',
