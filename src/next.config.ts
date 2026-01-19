93b960af07252724b08d07797c92bafde8869b5b
Imoudu Erua <imouduerua@gmail.com>
2026-01-19 13:42:05 +0000

    Server error: Credential implementation provided to initializeApp() via

---
+++ src/next.config.ts
@@ -0,0 +1,44 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    // Allow data URIs
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+   env: {
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: 'studio-4192523715-e9157',
+  },
+};
+
+export default nextConfig;
