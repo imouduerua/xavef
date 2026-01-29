272e83bafe727efb82b168c9f1b025c5c7fdc3fd
Imoudu Erua <imouduerua@gmail.com>
2026-01-29 12:19:52 +0000

    ok here's what i did, i created a new field named olidaraBalance in ever

--- src/app/(admin)/client-actions.ts
+++ src/app/(admin)/client-actions.ts
@@ -35,11 +35,11 @@ export async function updateTransactionStatusClient(
               balanceField = 'groupPoolBalance';
               break;
             default:
-              balanceField = 'solidaraBalance';
+              balanceField = 'olidaraBalance';
           }
           t.update(userRef, { [balanceField]: increment(amount) });
         } else if (txData.type === 'Withdrawal') {
-           t.update(userRef, { solidaraBalance: increment(-amount) });
+           t.update(userRef, { olidaraBalance: increment(-amount) });
         }
         t.update(transactionRef, { status: 'Completed' });
       } else if (decision === 'declined') {
